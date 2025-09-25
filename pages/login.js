6988f23aaa4f3dea71a951c8a6f6602bb7821da2
Atia As Samia <57853785+atiaassamia@users.noreply.github.com>
2025-09-25 16:43:46 +0600

    OrangHRM Playwright Project

--- pages/login.js
+++ pages/login.js
@@ -1,24 +1,25 @@
 exports.LoginPage = class LoginPage {
 
     constructor(page) {
-
-
         this.page = page;
 
-
-         // Click on Username field
+        // Click on Username field
         this.usernameFieldClick = page.getByRole('textbox', { name: 'Username' });
-         // Fill Username field
+        // Fill Username field
         this.usernameInput = page.getByRole('textbox', { name: 'Username' });
-        this.passwordFieldClick = page.getByRole('textbox', { name: 'Password' });
-        this.passwordInput = page.getByRole('textbox', { name: 'Password' });
-        this.loginButton = page.getByRole('button', { name: 'Login' });
 
+        // Click on Password field
+        this.passwordFieldClick = page.getByRole('textbox', { name: 'Password' });
+        // Fill Password field
+        this.passwordInput = page.getByRole('textbox', { name: 'Password' });
+
+        // click on Login Button
+        this.loginButton = page.getByRole('button', { name: 'Login' });
     }
 
 
-    async login(username, password) {
 
+    async login(username, password) {
 
         this.usernameFieldClick.click();
         await this.usernameInput.fill(username);
@@ -27,8 +28,10 @@ exports.LoginPage = class LoginPage {
         await this.passwordInput.fill(password);
 
         await this.loginButton.click();
-
-
     }
+
+
+
+
 
 }
